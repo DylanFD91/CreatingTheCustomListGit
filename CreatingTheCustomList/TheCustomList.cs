0c2bf77257afcc16dd0c29efc6d2201bc807d449
Dylan <dylan.f.derouchie@gmail.com>
2020-02-24 11:51:07 -0600

    added some test methods for positive and negative integers, as well as checking if objects will be on the proper index number.

---
+++ CreatingTheCustomList/TheCustomList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingTheCustomList
+{
+    public class TheCustomList<T>
+    {
+        public void Add(T item)
+        {
+            //increment count
+
+            //item needs to land at next available index
+
+            //check to make sure it persists
+
+            //count == maxcapacity, we need to increase capacity and copy
+        }
+    }
+}
